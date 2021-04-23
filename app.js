85082d757762ef7298061ba4db6e23a2e6724faf
Aangi Shah <aangishah2696@gmail.com>
2021-04-23 14:56:06 +0530

    Enviornment variables, cors and body parser added

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 
 const express = require('express');
 const routes = require('./routes');
+const dotenv=require('dotenv');
 
 const app = express();
+dotenv.config();
 
 app.use('/',routes);
 
-
- const server = app.listen(4000, function () {
- console.log('Server Lisening On Port : ' + "4000");
+const server = app.listen(process.env.PORT,function () {
+ console.log('Server Lisening On Port : ' + process.env.PORT);
  });
 
